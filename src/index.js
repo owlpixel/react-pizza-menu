529f571ea89e6faa5437668d02cbd81d46cbb113
Anowar <anrctg@gmail.com>
2023-11-09 02:51:45 +0600

    Co-authored-by: Anowar Hossen <anranik@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App(){
+    return <>
+                <h1>Hello react!</h1>
+                <Pizza />
+                <Pizza />
+                <Pizza />
+            </> 
+    
+} 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Pizza(){
+    return (
+        <>
+            <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci'/>
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </>
+    )
+    
+}
